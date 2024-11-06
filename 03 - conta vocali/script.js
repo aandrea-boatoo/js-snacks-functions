afbb56c09c50e5f99ac5bdb081220e0472f23ddd
Boato Andrea <163646394+aandrea-boatoo@users.noreply.github.com>
2024-11-06 13:54:30 +0100

    exercise 3

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -1,13 +1,22 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
 const word = 'javascript';
-
+const vowels = ["a", "e", "i","o", "u"]
 
 // Dichiara la funzione qui.
+function letterCounter(myString, myfilter){
+    let numLet = 0
+    for(let i= 0 ; i < myString.length ; i++){
+        if(myfilter.includes(myString[i])){
+            numLet += 1;
+        }
+    }
+    return numLet
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+const numVowels = letterCounter(word, vowels);
+console.log(numVowels);
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
